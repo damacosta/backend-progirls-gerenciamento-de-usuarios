cf5767047a79778eb76b90be387bb0ade52b99e8
Danielle Costa <d.mariano.costa@gmail.com>
2025-03-13 04:04:40 -0300

    :lock: fix(security): criptografar senhas com bcrypt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,39 @@
-exports.login = (req, res) => {
-    const { username, password } = req.body;
+const authService = require('../services/authService');
+const User = require('../models/User');
 
-    if (username === 'usuario' && password === 'senha') {
-        return res.status(200).json({ message: 'Login bem-sucedido!' });
-    } else {
-        return res.status(401).json({ message: 'Credenciais inválidas.' });
+async function register(req, res, next) {
+  console.log("Corpo da requisição:", req.body); // Linha adicionada
+
+  try {
+    const { cpf, nameComplete, situation, birthDate, email, phone, admissionDate, password } = req.body;
+    const hashedPassword = await authService.hashPassword(password);
+    const user = new User({ cpf, nameComplete, situation, birthDate, email, phone, admissionDate, password: hashedPassword });
+    await user.save();
+    res.status(201).send('Usuário registrado com sucesso!');
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send('Usuário não encontrado.');
     }
-};
+    const passwordMatch = await authService.comparePasswords(password, user.password);
+    if (passwordMatch) {
+      return res.status(200).send('Login realizado com sucesso!');
+    } else {
+      return res.status(401).send('Senha incorreta.');
+    }
+  } catch (error) {
+    next(error);
+  }
+}
 
-exports.recoverPassword = (req, res) => {
-    const { email } = req.body;
-
-    return res.status(200).json({ message: 'Instruções para redefinir a senha foram enviadas para o seu e-mail.' });
+module.exports = {
+  register,
+  login,
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const { ParseServer } = require('parse-server');
 const connectDB = require('./config/database');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -10,16 +9,6 @@ const errorHandler = require('./middlewares/errorMiddleware');
 dotenv.config();
 
 const app = express();
-
-const api = new ParseServer({
-    databaseURI: 'mongodb://admin:Y1QDz9L4hFclxrdaelLycUpF@MongoS3601A.back4app.com:27017/41fe412972294e73b1f6ad56a9501c1e',
-    cloud: './cloud/main.js',
-    appId: 'eAlcYywIW4MTRnn9A8Pq9zbFDIQnkwOKqKg10ZA0',
-    masterKey: 'Oge1DHN82TrRNkdrE2jkT1vXcitwmxdcStXw7Hvb',
-    serverURL: 'https://parseapi.back4app.com/'
-});
-
-app.use('/parse', api.app);
 
 app.get('/', (req, res) => {
     res.send('Backend rodando no Back4App!');
@@ -34,6 +23,6 @@ app.use('/api', authRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
